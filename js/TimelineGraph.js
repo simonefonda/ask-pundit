3056adec8ca2a7729cfa1132a509d03e0e77d69e
Christian Morbidoni <christian.morbidoni@gmail.com>
2012-09-22 18:59:47 +0300

    small fixes

--- js/TimelineGraph.js
+++ js/TimelineGraph.js
@@ -51,7 +51,6 @@ define(["dojo/_base/declare",
             
         }, // startup()
         
-
     	plotData: function(r, startDate, endDate) {
 		    
             var self = this;
@@ -122,7 +121,7 @@ define(["dojo/_base/declare",
     	        max = Math.max.apply(Math, data),
     	        Y = (height - bottomgutter - topgutter) / max;
 
-    	    r.drawGrid(leftgutter + X * .5 + .5, topgutter + .5, width - leftgutter - X, height - topgutter - bottomgutter, 10, 10, "#000");
+    	    //r.drawGrid(leftgutter + X * .5 + .5, topgutter + .5, width - leftgutter - X, height - topgutter - bottomgutter, 10, 10, "#fff");
 
     	    var path = r.path().attr({stroke: color, "stroke-width": 4, "stroke-linejoin": "round"}),
     	        bgp = r.path().attr({stroke: "none", opacity: .3, fill: color}),
@@ -157,7 +156,7 @@ define(["dojo/_base/declare",
     	                X0 = Math.round(leftgutter + X * (i - .5)),
     	                Y2 = Math.round(height - bottomgutter - Y * data[i + 1]),
     	                X2 = Math.round(leftgutter + X * (i + 1.5));
-    	            var a = getAnchors(X0, Y0, x, y, X2, Y2);
+    	            var a = self.getAnchors(X0, Y0, x, y, X2, Y2);
     	            p = p.concat([a.x1, a.y1, x, y, a.x2, a.y2]);
     	            bgpp = bgpp.concat([a.x1, a.y1, x, y, a.x2, a.y2]);
     	        }
@@ -233,7 +232,27 @@ define(["dojo/_base/declare",
             
             console.log('for num', num);
             
-    	}
+    	},
+
+		getAnchors: function(p1x, p1y, p2x, p2y, p3x, p3y) {
+		    var l1 = (p2x - p1x) / 2,
+		        l2 = (p3x - p2x) / 2,
+		        a = Math.atan((p2x - p1x) / Math.abs(p2y - p1y)),
+		        b = Math.atan((p3x - p2x) / Math.abs(p2y - p3y));
+		    a = p1y < p2y ? Math.PI - a : a;
+		    b = p3y < p2y ? Math.PI - b : b;
+		    var alpha = Math.PI / 2 - ((a + b) % (Math.PI * 2)) / 2,
+		        dx1 = l1 * Math.sin(alpha + a),
+		        dy1 = l1 * Math.cos(alpha + a),
+		        dx2 = l2 * Math.sin(alpha + b),
+		        dy2 = l2 * Math.cos(alpha + b);
+		    return {
+		        x1: p2x - dx1,
+		        y1: p2y + dy1,
+		        x2: p2x + dx2,
+		        y2: p2y + dy2
+		    };
+		}
         
 	});
 
