abb2317db4c228c03e8327fffb3e6d448d0f6832
Simone Fonda <fonda@netseven.it>
2012-09-24 21:37:36 +0200

    Annotation tag in progress

---
+++ js/TimelineAnnotationTag.js
@@ -0,0 +1,54 @@
+define(["dojo/_base/declare", 
+        "dojo/on", 
+        "dojo/text!ask/tmpl/TimelineAnnotationTagTemplate.html", 
+        "dijit/_WidgetBase", 
+        "dijit/_TemplatedMixin"], 
+    function(declare, on, timelineAnnotationTagTemplate, _WidgetBase, _TemplatedMixin) {
+	
+	return declare("ask.TimelineAnnotationTag", [_WidgetBase, _TemplatedMixin], {
+        notebookId: '',
+        label: '',
+        uri: '',
+        parentTimeline: '',
+        templateString: timelineAnnotationTagTemplate,
+        postMixInProperties: function() {
+            var self = this,
+                items = self.parentTimeline.notebookRawData.items,
+                _label = "http://www.w3.org/2000/01/rdf-schema#label",
+                foo;
+            
+            self.inherited(arguments);
+            
+            if (self.uri in items)
+                self.label = items[self.uri][_label][0].value;
+                
+            console.log('ANN tag post', self.label);
+            
+        },
+        startup: function() {
+            var self = this;
+            self.inherited(arguments);
+
+            /*
+            // Close annotation icon
+            on(dojo.query('.ti-ann-item[data-annotation="'+self.subject_enc+'"] .ti-ann-close'), 'click', function(e) {
+                dojo.query('.ti-ann-item[data-annotation="'+self.subject_enc+'"]').addClass('collapsed');
+            });
+
+            // Open annotation box
+            on(dojo.query('.box[data-target-annotation="'+self.subject_enc+'"]'), 'click', function(e) {
+                var foo = dojo.query('.ti-ann-item[data-annotation="'+self.subject_enc+'"]');
+                
+                // TODO: dojo .hasClass() ?
+                if (foo[0].className.match(/collapsed/) !== null) {
+                    dojo.query('.ti-ann-item').addClass('collapsed');
+                    foo.removeClass('collapsed');
+                } else {
+                    foo.addClass('collapsed');
+                }
+            });
+            */
+        }
+	});
+
+});
