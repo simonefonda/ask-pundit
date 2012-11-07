ef378d0726c0810488aebda6cc0d202c5831661d
Simone Fonda <fonda@netseven.it>
2012-11-07 10:14:42 +0100

    Refactored and cleaned up AuthenticatedRequester: * now has two methods login() and logout() to init the two procedures * the modal state involves the title as well * a new method isLoggedIn checks with the server if the user is logged

--- pundit18/AuthenticatedRequester.js
+++ pundit18/AuthenticatedRequester.js
@@ -8,6 +8,7 @@
  * authentication to work.
  */
 define([
+        "dojo/on",
         "dojo/_base/declare",
         "dojo/parser",
         "pundit/BaseComponent",
@@ -18,6 +19,7 @@ define([
     ], 
         
     function(
+        on,
         declare, 
         parser,
         BaseComponent,
@@ -30,7 +32,13 @@ define([
     return declare("pundit.AuthenticatedRequester", [BaseComponent, _WidgetBase, _TemplatedMixin], {
 
     templateString: LoginModalTemplate,
-    redirectURL: "http://metasound.dibet.univpm.it/annotationserver/login.jsp",
+    
+    // TODO: can be removed from here and just taken from the first auth request or
+    // is logged in
+    redirectURL: "AAAAAAAAAAAAAAhttp://metasound.dibet.univpm.it/annotationserver/login.jsp",
+    
+    _loggedIn: false,
+    _logginInAs: {},
     
     opts: {
         loginTimerMS: 500,
@@ -73,24 +81,71 @@ define([
         self.log('Authenticated requests component up and running!');
         
     },
+    
     startup: function() {
         var self = this;
         self.initLoginDialog();
-        
         self.log('Startup done');
+    },
+
+    /**
+    * @method xGet
+    * @description Performs an HTTP get through an authenticated Ajax call.
+    * @param options {object} the same object one would pass to a 
+    * normal dojo xhrGet().
+    */
+    xGet: function(callParams) {
+        return dojo.xhrGet(this._setWrappingCallParams(callParams));	
+    },
+	
+    /**
+    * @method xPost
+    * @description Performs an HTTP post through an authenticated Ajax call.
+    * @param options {object} the same object one would pass to a 
+    * normal dojo xhrPost().
+    */
+    xPost: function(callParams) {
+        dojo.xhrPost(this._setWrappingCallParams(callParams));
+    },
+	
+    /**
+    * @method xPut
+    * @description Performs an HTTP put through an authenticated Ajax call.
+    * @param options {object} the same object one would pass to a 
+    * normal dojo xhrPut().
+    */
+    xPut: function(callParams) {
+        dojo.xhrPut(this._setWrappingCallParams(callParams));
+    },
+	
+    /**
+    * @method xDelete
+    * @description Performs an HTTP delete through an authenticated Ajax call.
+    * @param options {object} the same object one would pass to a 
+    * normal dojo xhrDelete().
+    */
+    xDelete: function(callParams) {
+        dojo.xhrDelete(this._setWrappingCallParams(callParams));
     },
 
     initLoginDialog: function() {
         var self = this;
-        
-        dojo.connect(dojo.byId('pundit-login-open-button'), 'onclick', function() {
+
+        self._setLoginState('off');
+
+        on(dojo.byId('pundit-login-open-button'), 'click', function() {
             self.log('Opening the login dialog');
-            self.openLoginPopUp();
+            self._openLoginPopUp();
         });
         
-        dojo.connect(dojo.byId('pundit-login-close-button'), 'onclick', function() { 
+        on(dojo.byId('pundit-login-close-button'), 'click', function() { 
             self.log('Closing the login dialog');
             self.hideLogin();
+            clearTimeout(self.loginTimer);
+        });
+
+        // Clear the _checklogin timeout if the modal gets closed
+        on(dojo.byId('pundit-login-modal'), 'hidden', function() {
             clearTimeout(self.loginTimer);
         });
 	    
@@ -103,71 +158,38 @@ define([
     hideLogin: function() {
         dojo.query('#pundit-login-modal').modal('hide');
     },
-	
-    /**
-    * @method xGet
-    * @description Performs an HTTP get through an authenticated Ajax call.
-    * @param options {object} the same object one would pass to a 
-    * normal dojo xhrGet().
-    */
-    xGet: function(callParams) {
-        return dojo.xhrGet(this.setWrappingCallParams(callParams));	
-    },
-	
-    /**
-    * @method xPost
-    * @description Performs an HTTP post through an authenticated Ajax call.
-    * @param options {object} the same object one would pass to a 
-    * normal dojo xhrPost().
-    */
-    xPost: function(callParams) {
-        dojo.xhrPost(this.setWrappingCallParams(callParams));
-    },
-	
-    /**
-    * @method xPut
-    * @description Performs an HTTP put through an authenticated Ajax call.
-    * @param options {object} the same object one would pass to a 
-    * normal dojo xhrPut().
-    */
-    xPut: function(callParams) {
-        dojo.xhrPut(this.setWrappingCallParams(callParams));
-    },
-	
-    /**
-    * @method xDelete
-    * @description Performs an HTTP delete through an authenticated Ajax call.
-    * @param options {object} the same object one would pass to a 
-    * normal dojo xhrDelete().
-    */
-    xDelete: function(callParams) {
-        dojo.xhrDelete(this.setWrappingCallParams(callParams));
-    },
 
-    showLoginForm: function(redirectURL) {
-        var self = this;
-        
-        self.redirectURL = redirectURL;
-        dojo.query('#pundit-login-popup-content').removeClass('logged').removeClass('waiting');
-        self.showLogin();
-    },
-
-    openLoginPopUp : function() {
+    _openLoginPopUp: function() {
         var self = this;
 
         window.open(self.redirectURL, 'loginpopup', 'left=260,top=120,width=480,height=360');
 
-        dojo.query('#pundit-login-modal .modal-body')
-            .removeClass('pundit-login-state-off')
-            .addClass('pundit-login-state-waiting');
+        self._setLoginState('waiting');
+            
+        clearTimeout(self.loginTimer);
+        self._checkLogin();
+    },
+    
+    _setLoginState: function(s) {
+        dojo.query('#pundit-login-modal')
+            .removeClass('pundit-login-state-off pundit-login-state-waiting pundit-login-state-logged')
+            .addClass('pundit-login-state-'+s);
+    },
+    
+    _checkLogin: function() {
+        var self = this;
 
+        clearTimeout(self.loginTimer);
         self.loginTimer = setTimeout(function() {
-            self.checkLogin();
+            self.log('Not logged in.');
+            self.isLoggedIn(function(b) {
+                if (!b) self._checkLogin();
+            });
         }, self.opts.loginTimerMS);
-
+        
     },
 
-    checkLogin: function(f) {
+    isLoggedIn: function(f) {
         var self = this;
         
         var args = {
@@ -177,53 +199,43 @@ define([
                 "Accept":"application/json"
             },
             load: function(data) {
-                if (typeof(data) === 'undefined' || typeof(data.loginStatus) === 'undefined') { 
-                    data = {
-                        loginStatus: 0
-                    };
-                }
                 
-                if (typeof(f) === 'function') {
-                    f(data);
-                    return;
-                } else if (data.loginStatus == 0) {
-                    self.log('Not logged in.. yet...');
-                    self.loginTimer = setTimeout(function() {
-                        self.checkLogin();
-                    }, self.opts.loginTimerMS);
-                    return;
+                // If the json is not what we expect, normalize it a bit
+                if (typeof(data) === 'undefined' || typeof(data.loginStatus) === 'undefined') { 
+                    data = { loginStatus: 0 };
+                    
+                // If we see a .loginServer field: save it as redirectURL
+                } else if (typeof(data.loginServer) !== "undefined")
+                    self.redirectURL = data.loginServer;
+
+                // First time we see we're logged: fire the onLogin(), modify 
+                // the modal content etc. Same for logout.
+                if (data.loginStatus === 1) {
+                    if (self._loggedIn === false) self._afterLogin(data);
+                    if (typeof(f) === 'function') f(true, data);
+                } else {
+                    if (self._loggedIn === true) self._afterLogout();
+                    if (typeof(f) === 'function') f(false);
                 }
-                self.login(data);
+                return false;
+                
             },
             error: function(error) {}
         }
 
         self.xGet(args);
-    }, // checkLogin()
+    }, // isLoggedIn()
     
-    login: function(data) {
+    login: function() {
         var self = this;
         
-        dojo.query('#pundit-login-modal-title')
-            .html("You are now logged in!");
-
-        dojo.query('#pundit-login-modal .modal-body span.username')
-            .html(data.fullName+" ("+data.email+")");
-            
-        dojo.query('#pundit-login-modal .modal-body')
-            .removeClass('pundit-login-state-waiting')
-            .addClass('pundit-login-state-logged');
-        
-        for (var i = self.blockedRequests.length; i--;) 
-            self.xGet(self.blockedRequests[i]);
-      
-        self.fireOnLogin(data);
-        
-        self.log(data.fullName+" ("+data.email+")");
-        
-        setTimeout(function() { 
-            self.hideLogin();
-        }, self.opts.loginAutomaticHideMS);
+        // If we're logged in already, do nothing
+        self.isLoggedIn(function(b) {
+            if (b) return;
+            self.log('Login() opening the login window')
+            self._setLoginState('off');
+            self.showLogin();
+        })
         
     },
     
@@ -243,10 +255,8 @@ define([
                 if (typeof(data) !== 'undefined' && typeof(data.logout) !== 'undefined') {
                     data.msg = (data.logout == 1) ? 'Logged out succesfully' : 'You werent logged in.. and you still arent.';
 
-                    if (typeof(f) === 'function')
-                        f(data);
-
-                    self.fireOnLogout(data);
+                    if (typeof(f) === 'function') f(data);
+                    if (self._loggedIn === true) self._afterLogout(data);
                 }
             },
             error: function(error) {}
@@ -255,7 +265,51 @@ define([
         self.xGet(args);
     },
 
-    setWrappingCallParams : function(originalCallParams) {
+
+    // Automatically called when the login happens
+    _afterLogin: function(data) {
+        var self = this;
+
+        self.log("Logged in as: " + data.fullName+" ("+data.email+")");
+        
+        self._loggedIn = true;
+        self._loggedInAs = data;
+        
+        // Modify the modal: we are logged in
+        self._setLoginState('logged');
+        dojo.query('#pundit-login-modal .modal-body span.username')
+            .html(data.fullName+" ("+data.email+")");
+            
+        // exectue any pending blocked requests
+        for (var i = self.blockedRequests.length; i--;) 
+            self.xGet(self.blockedRequests[i]);
+      
+        // Hide the modal if open
+        if (dojo.hasClass('pundit-login-modal', 'in'))
+            setTimeout(function() { 
+                self.hideLogin();
+            }, self.opts.loginAutomaticHideMS);
+
+        self.fireOnLogin(data);
+        
+    },
+    
+    // Automatically called when the logout happens
+    _afterLogout: function(data) {
+        var self = this;
+
+        self._loggedIn = false;
+        self._loggedInAs = {};
+
+        dojo.query('#pundit-login-modal .modal-body span.username')
+            .html("");
+            
+        self._setLoginState('off');
+        self.fireOnLogout(data);
+        
+    },
+
+    _setWrappingCallParams : function(originalCallParams) {
         var self = this,
             wrappedParams = {
                 'withCredentials': true
@@ -269,7 +323,8 @@ define([
                 wrappedParams[key] = function(r) {
                     if (r && typeof(r.redirectTo) !== "undefined") {
                         self.blockedRequests.push(wrappedParams);
-                        self.showLoginForm(r.redirectTo);
+                        self.redirectURL = r.redirectTo;
+                        self.showLogin();
                     } else 
                         originalCallParams.load(r);
                 }
