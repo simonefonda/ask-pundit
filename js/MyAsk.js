20c46e59fd07f9cb7326dd93117e56be9264d48b
Simone Fonda <fonda@netseven.it>
2012-11-07 12:29:06 +0100

    * MyAsk login/logout buttons working * updating login info and buttons

--- js/MyAsk.js
+++ js/MyAsk.js
@@ -1,10 +1,11 @@
 define(["dojo/_base/declare", 
         "dojo/on", 
+        "dojo/dom-construct",
         "dojo/text!ask/tmpl/MyAskTemplate.html", 
         "ask/NotebookItem",
         "dijit/_WidgetBase", 
         "dijit/_TemplatedMixin"], 
-    function(declare, on, myAskTemplate, NotebookItem, _WidgetBase, _TemplatedMixin) {
+    function(declare, on, domConstruct, myAskTemplate, NotebookItem, _WidgetBase, _TemplatedMixin) {
 
     return declare("ask.MyAsk", [_WidgetBase, _TemplatedMixin], {
         templateString: myAskTemplate,
@@ -14,12 +15,75 @@ define(["dojo/_base/declare",
         startup: function() {
             var self = this;
             this.inherited(arguments);
-                 
-            self.getOwnedNotebooks(function(data) {
-                self.showOwnedNotebooks(data)
-            });
+            
+            self._isLoggedIn = false;
+            self._userData = {};
+            
+            self._initBehaviors();
         },
         
+        _initBehaviors: function() {
+            var self = this;
+            
+            ASK.requester.onLogin(function(d) {
+                self._afterLogin(d);
+            });
+            ASK.requester.onLogout(function(d) {
+                self._afterLogout(d);
+            });
+
+            // Are we logged in already? Just check.
+            // If not in logged in state already, go for it
+            ASK.requester.isLoggedIn(function(b, d) {
+                if (!self._isLoggedIn)
+                    self._afterLogin(d);
+            });
+
+            on(dojo.query('.ask-login')[0], 'click', function() {
+                ASK.requester.login();
+                return false;
+            });
+
+            on(dojo.query('.ask-logout')[0], 'click', function() {
+                ASK.requester.logout();
+                return false;
+            });
+
+        },
+
+        _afterLogin: function(d) {
+            var self = this;
+
+            self.getOwnedNotebooks(function(data) {
+                self.showOwnedNotebooks(data);
+            });
+
+            self._isLoggedIn = true;
+            self._userData = d;
+            
+            dojo.query('.my-ask')
+                .removeClass('login-state-off')
+                .addClass('login-state-logged');
+                
+            dojo.query('.my-ask .login-logged blockquote p')
+                .html('Logged in as '+d.fullName);
+            dojo.query('.my-ask .login-logged blockquote small')
+                .html(d.email);
+
+        },
+        
+        _afterLogout: function(d) {
+            var self = this;
+            
+            domConstruct.empty('my-ask-notebooks');
+            
+            self._isLoggedIn = false;
+            self._userData = {};
+            dojo.query('.my-ask')
+                .removeClass('login-state-logged')
+                .addClass('login-state-off');
+        },
+
         // TODO: switch pundit.AnnotationReader / Writer ? (Annotation ?? AnnotationServer ?)
         getOwnedNotebooks: function(cb){
             var self = this,
@@ -42,16 +106,19 @@ define(["dojo/_base/declare",
         
         showOwnedNotebooks: function(ids) {
 
+            domConstruct.empty('my-ask-notebooks');
+
             if (Object.prototype.toString.call(ids) !== '[object Array]') {
-                // not an array? errors
+                // TODO: not an array? errors
                 return;
             }
             
             var self = this;
-                            
+
             for (var j = ids.length; j--;) {
 
-                var foo = new NotebookItem({notebookId: ids[j]}).placeAt(dojo.byId('myAskContainer'));
+                var foo = new NotebookItem({notebookId: ids[j]})
+                    .placeAt(dojo.byId('my-ask-notebooks'));
 
                 // self.loadNotebooksMeta(data.NotebookIDs[i]);
                 
